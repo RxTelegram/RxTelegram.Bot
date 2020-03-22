54d7bf18705b9dd54ead55e840b83ef0ebe630aa
Niklas Weimann <nikweimann@gmail.com>
2020-03-22 13:01:41 +0100

    Add BotInfo

---
+++ UnitTests/BotInfoTest.cs
@@ -0,0 +1,29 @@
+using Core;
+using Core.Exceptions;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class BotInfoTest
+	{
+		[Test]
+		public void ValidToken()
+		{
+			var botInfo = new BotInfo("123456:ABC-DEFG1234ghIkl-zyx57W2v1u123ew11");
+			Assert.IsNotNull(botInfo);
+			Assert.That(botInfo.Id, Is.EqualTo(123456));
+			Assert.That(botInfo.Token, Is.EqualTo("123456:ABC-DEFG1234ghIkl-zyx57W2v1u123ew11"));
+			Assert.That(botInfo.BotUrl, Is.EqualTo("https://api.telegram.org/bot123456:ABC-DEFG1234ghIkl-zyx57W2v1u123ew11/"));
+			Assert.That(botInfo.BotFileUrl, Is.EqualTo("https://api.telegram.org/file/bot123456:ABC-DEFG1234ghIkl-zyx57W2v1u123ew11/"));
+		}
+
+		[Test]
+		public void InvalidToken() =>
+			Assert.Throws<InvalidTokenException>(() => { new BotInfo("a123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"); });
+
+		[Test]
+		public void InvalidToken2() =>
+			Assert.Throws<InvalidTokenException>(() => { new BotInfo("123456:?????"); });
+	}
+}
