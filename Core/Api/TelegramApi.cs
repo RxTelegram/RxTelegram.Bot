1703b9bbab7c8a5f4aa1c6a43213440dd5fd079a
Ramon Ising <r-ising@users.noreply.github.com>
2020-03-26 20:07:13 +0100

    Implement some telegram methods

--- Core/Api/TelegramApi.cs
+++ Core/Api/TelegramApi.cs
@@ -17,9 +17,20 @@ namespace Core.Api
 
         public Task<Message> SendPhoto(SendPhoto message) => Post<Message>("sendPhoto", message);
 
+        public Task<Message> SendAudio(SendAudio message) => Post<Message>("sendAudio", message);
+
         public Task<Message> SendDocument(SendDocument message) => Post<Message>("sendDocument", message);
+
+        public Task<Message> SendVideo(SendVideo message) => Post<Message>("sendVideo", message);
+
+        public Task<Message> SendAnimation(SendAnimation message) => Post<Message>("sendAnimation", message);
+
+        public Task<Message> SendVoice(SendVoice message) => Post<Message>("sendVoice", message);
+
+        public Task<Message> SendVideoNote(SendVideoNote message) => Post<Message>("sendVideoNote", message);
 
         public Task<Message> SendMediaGroup(SendMediaGroup message) => Post<Message>("sendMediaGroup", message);
 
+        public Task<Message> SendLocation(SendLocation message) => Post<Message>("sendLocation", message);
     }
 }
